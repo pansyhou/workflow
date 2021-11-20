db001e8d75598f583fb2488a58d8aa1b7317eeea
pansy <1677195845@qq.com>
2021-11-20 13:22:13 +0800

    注释

--- exp5/exp5_5.c
+++ exp5/exp5_5.c
@@ -5,16 +5,16 @@ void main()
     char a[1000];
     gets(a);
     int len=strlen(a);
-    strlwr(a);//97-122
-    int num[9]={0};//48-57
-    int alpha[25]={0};
+    strlwr(a);//全部转成小写
+    int num[9]={0};//数字ASCII码范围48-57,定义数字计数器数组
+    int alpha[25]={0};//97-122定义字母计数器数组
     int i=0;
     for (;i<len;i++)
     {
         if(a[i]>47&&a[i]<58)
         {
-            int temp=a[i]-48;
-            num[temp]++;
+            int temp=a[i]-48;//将字符的ascii码转成数字
+            num[temp]++;//对应下标计数器加一
         }
         if (a[i]>96&&a[i]<123)
         {
@@ -24,7 +24,7 @@ void main()
     }
     for (int i=0;i<10;i++)
     {
-        printf("%c出现%d次\n",i+48,num[i]);
+        printf("%c出现%d次\n",i+48,num[i]);//i+48转成ascii码输出
     }
     for (int i=0;i<26;i++)
     {
--- exp5/exp5_6.c
+++ exp5/exp5_6.c
@@ -2,5 +2,12 @@
 void main()
 {
     int stu_num=0;
-    int i=0;
+    int i=0,s=0;
+    int mark[100];
+    for (;i<100&&s!='\n';i++)
+    {
+        scanf("%d",&mark[i]);
+        s=getchar();
+        stu_num++;
+    }
 }
